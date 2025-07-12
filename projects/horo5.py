32e7e21f150152e9db5556ba12bf47230fea804c
athenova <aleksey.sergeyev@yandex.com>
2025-07-12 15:24:55 +0300

    В пт вместо гороскопа будет послание дня

---
+++ projects/horo5.py
@@ -0,0 +1,53 @@
+from datetime import datetime, timedelta
+from simple_blogger.poster.telegram import TelegramPoster
+from simple_blogger.poster.vk import VkPoster
+from simple_blogger.blogger import SimplestBlogger
+from simple_blogger.builder import PostBuilder
+from simple_blogger.builder.content import ContentBuilder
+from simple_blogger.generator.deepseek import DeepSeekTextGenerator
+from simple_blogger.generator.yandex import YandexTextGenerator
+from simple_blogger.generator.openai import OpenAiTextGenerator
+from simple_blogger.preprocessor.text import TagAdder
+from simple_blogger.builder.prompt import IdentityPromptBuilder
+from datetime import datetime, timedelta, date
+
+class HoroscopeBlogger(SimplestBlogger):
+    def __init__(self, sign, tg_chat_id, vk_group_id):
+        tomorrow = datetime.today() + timedelta(days=1)
+        builder = PostBuilder(
+            message_builder=ContentBuilder(
+                generator=OpenAiTextGenerator(system_prompt=f"Ты - профессиональный астролог"),
+                prompt_builder=IdentityPromptBuilder(f"Напиши послание дня для знака '{sign}' на {tomorrow.strftime('%Y-%m-%d')}. Используй структуру: послание дня, аффирмация дня")
+            )
+        )
+        processor = TagAdder(['#гороскоп', '#астрология', '#посланиедня', f"#{sign}"])
+        posters = [
+            TelegramPoster(chat_id=tg_chat_id, processor=processor),
+            VkPoster(group_id=vk_group_id, processor=processor)
+        ]
+        super().__init__(builder, posters)
+
+def post():
+    # bloggers = [
+    #     HoroscopeBlogger(sign='рыбы', tg_chat_id=None, vk_group_id=None),
+    # ]
+    bloggers = [
+        HoroscopeBlogger(sign='рыбы', tg_chat_id='@pisces_the', vk_group_id='229837683'),
+        HoroscopeBlogger(sign='овен', tg_chat_id='@aries_the', vk_group_id='229837854'),
+        HoroscopeBlogger(sign='телец', tg_chat_id='@horo_ai', vk_group_id='229860740'),
+        HoroscopeBlogger(sign='близнецы', tg_chat_id='@gemini_the', vk_group_id='229837895'),
+        HoroscopeBlogger(sign='рак', tg_chat_id='@horo_ai', vk_group_id='229860780'),
+        HoroscopeBlogger(sign='лев', tg_chat_id='@horo_ai', vk_group_id='229860665'),
+        HoroscopeBlogger(sign='дева', tg_chat_id='@horo_ai', vk_group_id='229860810'),
+        HoroscopeBlogger(sign='весы', tg_chat_id='@horo_ai', vk_group_id='229860834'),
+        HoroscopeBlogger(sign='скорпион', tg_chat_id='@horo_ai', vk_group_id='229860866'),
+        HoroscopeBlogger(sign='стрелец', tg_chat_id='@horo_ai', vk_group_id='229860894'),
+        HoroscopeBlogger(sign='козерог', tg_chat_id='@capricorn_the', vk_group_id='229837876'),
+        HoroscopeBlogger(sign='водолей', tg_chat_id='@aquarius_the', vk_group_id='229837930'),
+    ]
+
+    for blogger in bloggers:
+        blogger.post()
+
+if __name__ == "__main__":
+    post()
